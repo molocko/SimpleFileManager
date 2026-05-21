590e49cdaaa2412dfa58cc8c66fe182bdf4abe73
DirtyMilk <64641355+molocko@users.noreply.github.com>
2026-05-21 12:55:01 +0300

    Add files via upload

--- CLI/start.cpp
+++ CLI/start.cpp
@@ -1,10 +1,8 @@
-//#include "include/fmt/base.h"
 #include <vector>
 #include <iostream>
 #include <conio.h>
 //#include <windows.h>
 #include <filesystem>
-//#pragma comment(lib, "CLI/include/fmt.lib")
 
 #define YELLOW "\x1b[33m"
 #define BLACK "\x1b[30m"
@@ -19,13 +17,12 @@
 void resetCur(int64_t&);
 void loadDirectories(std::vector<std::filesystem::path>&, const std::filesystem::path&);
 void clearBuffer(std::string&);
+void renderAllSh(std::string&);
 //void clear();
 
-//using fmt::print;
-//using std::cout;
-//using std::endl;
 
-//int rows{12};
+int rows{12};
+
 
 std::string buffer{};
 
@@ -35,21 +32,25 @@ void start() {
 	int64_t cur{};
 	std::vector<std::filesystem::path> list;
 
-	//std::filesystem::path path = std::filesystem::current_path();
 	std::filesystem::path path = std::filesystem::current_path().string() + '\\';
 
+	bool scrollAllScene{ false };
 	int scan{};
 	resetCur(cur);
 	loadDirectories(list, path);
+
+	int temp{};
 	while (1) {
 		clearBuffer(buffer);
 		//system("cls");
 		//clear();
-		if (scan == 13) {		// ENTER
+
+		// ===============================
+		if (scan == 13) {		// ENTER 10
 			if (cur == -1) {
-				
-				std::string ttt = std::string(path.string().c_str(), path.string().substr(0,path.string().size()-1).find_last_of('\\'));
-				std::filesystem::path temp = std::filesystem::path(ttt+'\\');
+
+				std::string ttt = std::string(path.string().c_str(), path.string().substr(0, path.string().size() - 1).find_last_of('\\'));
+				std::filesystem::path temp = std::filesystem::path(ttt + '\\');
 				//print(" || {} || ", );
 
 
@@ -65,71 +66,59 @@ void start() {
 					resetCur(cur);
 					loadDirectories(list, temp);
 				} else {
+
+					// TODO: ОТМЕНЯТЬ ПЕРЕРЕНДЕР А ТО ОН РЕНДЕРИТ ВСЕ НАХУЯЯ
 					system(("start " + temp.string()).c_str());
+					//scan = 0;
+					//continue;
 				}
 			}
-		}; 
+		};
 		if (scan == 27) return; // ESC
-		if (scan == 72) {		// UP
-			if(cur > -1) cur--;
+		if (scan == 72) {		// UP 119
+			if (cur > -1) cur--;
 		}
-		if (scan == 80) {		// DOWN
-			if(cur < static_cast<int64_t>(list.size())-1) cur++;   // !!!!!!!!!!!!!!!!!!!
+		if (scan == 80) {		// DOWN 115
+			if (cur < static_cast<int64_t>(list.size()) - 1) cur++;   // !!!!!!!!!!!!!!!!!!!
 		}
 
-
-
-		//print("Current directory:\n{}\n\n", path.string());
-		//printf("Current directory:\n%s\n\n", path.string().c_str());
+		// ===============================
 		buffer += "Current directory:\n" + path.string() + "\n\n";
+
 		if (path == path.root_path()) {
 			if (cur == -1) cur++;
+			//temp--;
 			//cur = 0;
 		} else {
-			//if (cur == -1) printf(CURSOR "<--\n");
-			if (cur == -1) buffer+=CURSOR "<--\n";
+			if (cur == -1) buffer += CURSOR "<--\n";
 
-			//else printf("<--\n");
-			else buffer+="<--\n";
-			
+			else buffer += "<--\n";
+
 		}
+		// ============= All to buffer =============
 		for (size_t i{}; i < list.size(); i++) {
 			if (cur == i) {
-				//if(std::filesystem::directory_entry(list.at(i)).is_directory())
-				if(std::filesystem::is_directory(list.at(i)))
-					//printf(YELLOW_BG BLACK CURSOR);
+				if (std::filesystem::is_directory(list.at(i)))
 					buffer += YELLOW_BG BLACK CURSOR;
 				else
-					//printf(WHITE_BG  BLACK  CURSOR);
-					buffer+=(WHITE_BG  BLACK  CURSOR);
+					buffer += (WHITE_BG  BLACK  CURSOR);
 				//continue;
 			} else {
 				if (std::filesystem::is_directory(list.at(i)))
-					//printf(YELLOW);
-					buffer+=(YELLOW);
+					buffer += (YELLOW);
 			}
 
-			
 
-			try {					/// IDKKK как фикс это
-				//printf("%s\x1b[0m\n", list.at(i).filename().string().c_str());
-				buffer += (list.at(i).filename().string()+"\x1b[0m\n");
 
-			} catch (...) {
-				//std::cout << list.at(i).filename().string() << std::endl;
-				std::cout << "111111111111111111111111111111111111111" << std::endl;
-			}
+			buffer += (list.at(i).filename().string() + "\x1b[0m\n");
+
 		}
-		printf("%s%s%s", CLEAR, buffer.c_str(), "\x1B[?25l");
+		// ===============================
+		renderAllSh(buffer);
 
 		scan = _getch();
 
-
 	}
-
-	
-	//using namespace std;
-	//std::this_thread::sleep_for(1s);
 };
 
 void loadDirectories(std::vector<std::filesystem::path>& vec, const std::filesystem::path& path) {
@@ -145,6 +134,10 @@ void resetCur(int64_t& cur) {
 void clearBuffer(std::string& buf) {
 	buf.clear();
 }
+
+void renderAllSh(std::string& buf) {
+	printf("%s%s%s", CLEAR, buffer.c_str(), "\x1B[?25l");
+};
 
 
 //void clear() {
